a5113b5486ebe7bd421e6aaa7ecd1f75621579cc
Kauezs <kauecardosocruz@gmail.com>
2024-12-03 09:40:59 -0300

    Adicionei os arquivos da aula 02

---
+++ Aula 02/Aula02.py
@@ -0,0 +1,11 @@
+print('Hoje é dia 03 de Dezembro')
+
+nome_usuario = 'Kaue Cardoso' # tipo -> str = string
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.81 # tipo -> float = ponto flutuante 
+maior_idade = False # tipo -> bool = boolean
+
+print('Seja bem-vindo', nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+print('Você tem', altura_usuario, 'metros')
+print('Você é maior de idade?', maior_idade)
---
+++ Aula 02/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+print('Hoje é dia 03 de Dezembro')
+
+nome_usuario = 'Kaue Cardoso' # tipo -> str = string
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.80 # tipo -> float = ponto flutuante 
+maior_idade = False # tipo -> bool = boolean
+
+print('Seja bem-vindo', nome_usuario)
